ad7a81510724e735d12fd768bbdcaee7feeb1018
Samuel Reis <reis.samuel@estudante.ufjf.br>
2023-11-28 22:57:18 -0300

    criado estoque

---
+++ estoque.js
@@ -0,0 +1,18 @@
+let estoque = {
+    'joao': {
+        'maca': 1,
+        'banana': 2
+    },
+    'maria':{
+        'maca': 2,
+        'pera': 3
+    }
+};
+
+
+function getEstoque(){
+    return structuredClone(estoque);
+}
+
+
+export {getEstoque};
